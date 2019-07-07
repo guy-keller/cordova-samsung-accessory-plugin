c175339471c08c05c3337d1ab4094868ab59ad40
Gui Keller <guikeller@gmail.com>
2019-07-07 19:25:22 +0930

    the plugin is now a consumer as it makes more sense for it to be

--- www/cordova-samsung-accessory-plugin.js
+++ www/cordova-samsung-accessory-plugin.js
@@ -5,6 +5,9 @@ module.exports = {
     shutdown: function(onNewMessageCallback, errorCallback) {
         cordova.exec(onNewMessageCallback, errorCallback, "CordovaSamsungAccessoryPlugin", "shutdown", []);
     },
+    findPeer: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "CordovaSamsungAccessoryPlugin", "findPeer", []);
+    },
     sendMessage: function(successCallback, errorCallback, message) {
         cordova.exec(successCallback, errorCallback, "CordovaSamsungAccessoryPlugin", "sendMessage", [message]);
     },
